6574017c2e211419f8faa65a257e2bcc26588628
ThomasAribart <thomasa@theodo.fr>
2024-08-21 15:45:40 +0200

    export onion class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export type { Onion } from './onion.js'
-export type { Layer, Type, Types, OutwardFn, OutwardFns, InwardFn, InwardFns } from './layer.js'
+export { Onion } from './onion.js'
 export { compose } from './compose.js'
+export type { Layer, Type, Types, OutwardFn, OutwardFns, InwardFn, InwardFns } from './layer.js'
